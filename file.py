53568cdfd93d6a8c5704fe02b6c08eb7d496e801
Vaibhav Sai Gandi <vaibhavsaigandi@gmail.com>
2025-11-13 14:34:14 -0500

    first message

---
+++ file.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+## Title of the aplication
+st.title("Hello Streamlit")
+
+## Diplay a Simple Text
+st.write("This is a simple text")
